c018e5c53704dd5df69d05ac6b96f471a0f092ea
ryota-aus <74895872+ryota-aus@users.noreply.github.com>
2020-12-14 15:49:26 +0900

    本番環境修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  
   
   def index
     @prototypes = Prototype.all
@@ -29,6 +29,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
  
   def update
@@ -51,10 +54,6 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :edit
-    end
-  end
+  
 end
 
